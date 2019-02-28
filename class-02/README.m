ec245a0db435daa8a9fd684b1e8a733e175c0172
Carson McKinstry <carson.mckinstry@gmail.com>
2019-02-28 11:07:16 -0600

    Create README.m

---
+++ class-02/README.m
@@ -0,0 +1,34 @@
+# Class 2 - State Review, Lifecycle Methods, and HOOKS!
+
+## State
+
+How do we handle component state in React?
+
+* constructor
+* this.state
+* this.setState
+
+## Lifecycle Methods
+
+Methods for hooking into different events happening with your component
+
+* componentDidMount
+* componentWillUnmount
+* componentDidUpdate
+* Legacy methods - avoid these in your code, you don't need them anymore
+    * componentWillMount
+    * componentWillUpdate
+    * componentWillReceiveProps    
+
+## React Hooks
+
+Or, the new way to do things
+
+* no more class components, only functions!
+* useState
+* useEffect
+
+if we have time, otherwise we'll go over these next week
+
+* useRef
+* useReducer
